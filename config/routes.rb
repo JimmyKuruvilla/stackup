48299d3bf66af12c5d8f15c543749b3978947541
seemajune <seemashariat@gmail.com>
2014-11-24 16:03:17 -0500

    finished controllers for show page upon sign up

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :users
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  #   get 'users/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
