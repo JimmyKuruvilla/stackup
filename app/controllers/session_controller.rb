49332a1684a185e66f21d868445f33fc28943f01
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-11-26 10:12:32 -0500

    done

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,10 +2,10 @@ class SessionController < ApplicationController
 
   def create     
     auth = request.env["omniauth.auth"]     
-    if User.find_by_uid(auth["uid"])==nil
+    if User.find_by(uid: auth["uid"])==nil
       message="You can expect your first StackUp email to appear within 24 hours"
     end
-    @user = User.find_by_uid(auth["uid"]) || User.create_with_omniauth(auth)     
+    @user = User.find_by(uid: auth["uid"]) || User.create_with_omniauth(auth)     
     session[:user_id] = @user.id     
 
     redirect_to @user, :notice => message
