519f5667434d060d7ad28e3d6a6e7717feb6614d
Jimmy Kuruvilla <jimmyjk@gmail.com>
2014-11-26 12:24:45 -0500

    fixed destroy users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,6 +19,8 @@ end
   def destroy_all_users #and make admins
     User.destroy_all
     User.create(id: 1, email: "jimmy.kuruvilla@flatironschool.com", created_at: "2014-11-26 04:15:53", updated_at: "2014-11-26 04:16:22", uid: "7905473", name: "Jimmy Kuruvilla", admin: true)
+    User.create(id: 4, email: "seemashariat@gmail.com", created_at: "2014-11-26 17:16:06", updated_at: "2014-11-26 17:16:06", uid: "2113081", name: "seema shariat", admin: true)
+    User.create(id: 5, email: "doneallison@gmail.com", created_at: "2014-11-26 17:18:09", updated_at: "2014-11-26 17:18:09", uid: "3227364", name: "Don Allison", admin: nil)
     redirect_to root_path, :notice => "All users destroyed, and admins recreated"
   end
 
